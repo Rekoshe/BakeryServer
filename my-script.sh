1d308184dcccca087bb4422d545a900f1edac493
Rekoshe <42325581+Rekoshe@users.noreply.github.com>
2024-09-30 12:27:14 +0100

    changed the hompage in packagejson file and built

--- my-script.sh
+++ my-script.sh
@@ -2,10 +2,11 @@
 if [ -d "./../../ExpressApps/BakeryServer/docs" ]; then
 
     rm -rv ./../../ExpressApps/BakeryServer/docs/*
-    cp -v build/* ./../../ExpressApps/BakeryServer/docs/
+    cp -rv build/* ./../../ExpressApps/BakeryServer/docs/
+    cp -rv build/* docs/
 
 #Else make a new folder and then move the files.
 else
     mkdir -p ./../../ExpressApps/BakeryServer/docs
-    cp -v build/* ./../../ExpressApps/BakeryServer/docs/
+    cp -rv build/* ./../../ExpressApps/BakeryServer/docs/
 fi
